7dee97dfd20510b65c7369b0d516ea6d71d4dd3a
Gautham Kolluru <gautham.kolluru@gmail.com>
2018-09-11 18:55:03 +0530

    Useful SQL Queries

---
+++ useful_sql/SQLQuery2.sql
@@ -0,0 +1,13 @@
+SELECT * FROM tblArticles
+
+SELECT * FROM tblArticleCategories
+
+SELECT * FROM tblArticleSubCategories WHERE SubCategoryID = 13
+
+SELECT * FROM tblArticles_SubCategories WHERE SubCategoryID = 13 --Relationship Table between 
+
+SELECT * FROM tblArticleKeywords
+
+SELECT * FROM tblArticle_Keyword
+
+SELECT * FROM tblArticles WHERE ArticleID = 54
---
+++ useful_sql/SQLQuery5.sql
@@ -0,0 +1,13 @@
+SELECT * FROM tblEmailMessage
+
+SELECT COUNT(*), Address FROM tblUserEMail GROUP BY Address
+
+SELECT * FROM tblUserEMail WHERE Address = 'nseeram@gdresearchcenter.com'
+
+SELECT 
+--COUNT(*)
+* 
+FROM 
+tblUsers 
+WHERE 
+UserName = 'skolluru'
